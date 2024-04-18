5286d5ac8d96a8d4de2b4537c2106004635dee43
Tamil Iniyan S <tamiliniyans1999@gmail.com>
2024-04-18 09:57:48 +0530

    Minor fixes done

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,8 @@ export const App = () => {
           <div className={styles.info}>
             <h2 className={styles.title}>Great</h2>
             <p className={styles.description}>
-              Your performance exceed 65% of the people conducting the test
-              here!
+              You scored higher than 65% of the people who have taken these
+              tests.
             </p>
           </div>
         </div>
