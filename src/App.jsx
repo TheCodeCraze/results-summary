b23d81c91a8c2ab3d6466e8b5b0f6671149175cb
Tamil Iniyan S <tamiliniyans1999@gmail.com>
2024-04-18 08:21:35 +0530

    Responsive design completed

--- src/App.jsx
+++ src/App.jsx
@@ -11,54 +11,48 @@ export const App = () => {
       .then((value) => setData(value));
   }, []);
 
-  return (
+  return data ? (
     <main className={styles.container}>
-      {!data ? (
-        <p className={styles.loading}>Loading...</p>
-      ) : (
-        <>
-          <div className={styles.result}>
-            <div className={styles.content}>
-              <h1 className={styles.title}>Your Result</h1>
-              <div className={styles.score}>
-                <p className={styles.current}>76</p>
-                <p className={styles.total}>of 100</p>
-              </div>
-              <div className={styles.info}>
-                <h2 className={styles.title}>Great</h2>
-                <p className={styles.description}>
-                  Your performance exceed 65% of the people conducting the test
-                  here!
-                </p>
-              </div>
-            </div>
+      <div className={styles.result}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Your Result</h1>
+          <div className={styles.score}>
+            <p className={styles.current}>76</p>
+            <p className={styles.total}>of 100</p>
           </div>
-          <div className={styles.summary}>
-            <h2 className={styles.title}>Summary</h2>
-            <div className={styles.categories}>
-              {data.map((item) => (
-                <div
-                  key={item.category}
-                  className={`${styles.category} ${styles[item.category]}`}
-                >
-                  <div className={styles.name}>
-                    <img
-                      src={item.icon}
-                      alt={`${item.category} category representation`}
-                      className={styles.icon}
-                    />
-                    <p className={styles.text}>{item.category}</p>
-                  </div>
-                  <p className={styles.score}>
-                    <span>{item.score}</span> / 100
-                  </p>
-                </div>
-              ))}
-            </div>
-            <button className={styles.button}>Continue</button>
+          <div className={styles.info}>
+            <h2 className={styles.title}>Great</h2>
+            <p className={styles.description}>
+              Your performance exceed 65% of the people conducting the test
+              here!
+            </p>
           </div>
-        </>
-      )}
+        </div>
+      </div>
+      <div className={styles.summary}>
+        <h2 className={styles.title}>Summary</h2>
+        <div className={styles.categories}>
+          {data.map((item) => (
+            <div
+              key={item.category}
+              className={`${styles.category} ${styles[item.category]}`}
+            >
+              <div className={styles.name}>
+                <img
+                  src={item.icon}
+                  alt={`${item.category} category representation`}
+                  className={styles.icon}
+                />
+                <p className={styles.text}>{item.category}</p>
+              </div>
+              <p className={styles.score}>
+                <span>{item.score}</span> / 100
+              </p>
+            </div>
+          ))}
+        </div>
+        <button className={styles.button}>Continue</button>
+      </div>
     </main>
-  );
+  ) : null;
 };
